34ad610e76fcaefce16e4bfabd902887eb096248
Hash <c3256011@uon.edu.au>
2020-12-10 20:52:38 +1100

    merging

--- Matlab/nonlcon.m
+++ Matlab/nonlcon.m
@@ -2,8 +2,8 @@ function [c,ceq] = nonlcon(U,qgoal, q0,H,tspan)
 
 global m1 m2
 
-tolerance = 0.05;
-
 ceq = [];
+c = [abs(q0(1)*m1+0.5 - q0(2)*m2-0.08)-0.2 ];
+% ; abs(q0(5:8))-0.5
 c=[q0(1)*m1 - q0(2)*m2 ];
 end
