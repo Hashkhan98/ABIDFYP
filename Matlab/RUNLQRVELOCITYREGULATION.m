b7cbe46e8499c40fcb0049957cb60399574a8ed0
Hash <c3256011@uon.edu.au>
2020-12-11 12:11:15 +1100

    velocity

--- Matlab/RUNLQRVELOCITYREGULATION.m
+++ Matlab/RUNLQRVELOCITYREGULATION.m
@@ -5,11 +5,11 @@ linflag = 1;  %% 0 for Linear ; 1 for NonLinear
 %% Declare Constants
 Parameters;
 
-%%Weights for Q and R for each state
-Q = diag([10 10 10 10 1 1 1 1]);
+%%Weights for Q and R for each stat     e
+Q = diag([1e8 1e8 1e8 1e5 1 1 1 1]);
 R = diag(1);
 
-q0 = [0.8000; 0.2222; 0; 0.9000; 0; 0; 0; 0];
+q0 = [0.3536; 0.2149; -0.7854; 0.4000; 0; 0; 0; 0];
 u0 = [0 0 0 0]';
 
 % qstar = [0.7,0.8*m1/m2,20*pi/180,0.8 ,0,0,0,0]';
@@ -68,7 +68,7 @@ param.B = [zeros(4,4); diag([1/m1 ; 1/m2 ; 1 ; 1])];
 XX = [];
 UU = [];
 
-simtime = 5; % Length of simulation in seconds
+simtime = 20; % Length of simulation in seconds
 T = 0.01; % 
 simstep = [0 T];
 
@@ -76,15 +76,19 @@ qall = zeros(8,simtime/T);
 qall(:,1) = q0;
 
 time = 0:T:simtime;
-% [tx,xx] = ode45(@(t,q) LQR(t,q,K),tspan,q0); 
-[qstar,qstardot,qstardotdot] = TrajectoryGeneration(time); 
+%% Trajectory 
+wpts = [0.25 +0.5 0.5 0.25 0.25;
+        0.25 +0.25 0.5 0.5 0.25;
+        0.4 0.4 0.4 0.4 0.4];
+    
+maxvel = 0.25;
+[qstar,qstardot,qstardotdot] = TrajectoryGeneration(wpts,time,maxvel); 
 
 %% SIM
 tic
 for i = 1:(simtime/T)  
 %     qall(:,i)=massmatrix(qall(:,i-1),u(:,i-1),tspan)';
-    [xx] = ode4(@(t,q) LQR(t,q,K,qstar,qstardot,qstardotdot,linflag),time,qall(:,i));
-%     xx = Model(i,qall(:,i),K,qstar,qstardot,qstardotdot,linflag);
+    [~,xx] = ode45(@(t,q) LQR(t,q,K,qstar,qstardot,qstardotdot,linflag,i),simstep,qall(:,i));
     qall(:,i+1) = xx(end,:)';
 
 end
@@ -112,61 +116,26 @@ legend('dr1','dr2','dtheta','dz','des dr1','des dr2','des dtheta','des dz')
 set(gcf,'color','w')
 set(gca,'fontweight','bold','fontsize',11)
 
-% figure(3)
-% plot(time,-K*(qall-repmat(qstar,1,length(time))))
-% set(gcf,'color','w')
-% set(gca,'fontweight','bold','fontsize',11)
-% figure(2)
-% plot(t,XX(1,:),t,XX(2,:),t,XX(3,:),t,XX(4,:))
+figure(3)
+plot(time(1:length(time)-1),param.Uplot(1:3,:))
+set(gcf,'color','w')
+set(gca,'fontweight','bold','fontsize',11)
   
-% function dx = Model(t,q,K,qstar,qstardot,qstardotdot,linflag)
-% 
-% global param
-% Parameters;
-% t = t + 1;
-% param.U = -K*q;
-% %ref tracking
-% U1 = -K*(q - [qstar(:,t);qstardot(:,t)]);
-% 
-% U2 = param.B \ (-param.A*[qstar(:,t);qstardot(:,t)] + [qstardot(:,t);qstardotdot(:,t)]);
-% 
-% param.U = U1 + U2;
-% 
-% param.U = param.U + [0;0;0;g*(m1+m2+mbh)];  %gravity cancellation
-% 
-% if linflag
-%     %feedback linearisation
-%     fx = [q(5:8);0;0;0;0];
-%     gx = [0;0;0;0;(param.U(1) + m1*q(1)*q(7)^2)/m1;...
-%           (param.U(2) + m2*q(2)*q(7)^2)/m2;...
-%           (param.U(3)  - (2*m1*q(1)*q(5)*q(7) + 2*m2*q(2)*q(6)*q(7)))/(m1*q(1)^2 + m2*q(2)^2 + Ib);...
-%           (param.U(4))/(m1+m2+mbh) - g];
-%     dx = fx + gx;
-% else
-%     ddq = inv(param.M) * (-param.C*q(5:8) - param.G + param.U);
-%     dx = [q(5:8);ddq];
-% end
-%% trajectory stuff
-% trajectoryOutputHelper(t);
-%%
 
-% end
-
-function dx = LQR(t,q,K,qstar,qstardot,qstardotdot,linflag)
+function dx = LQR(t,q,K,qstar,qstardot,qstardotdot,linflag,i)
 
 global param
 Parameters;
 
-t = t*100 + 1;
 
 %ref tracking
-U1 = -K*(q - [qstar(:,t);qstardot(:,t)]);
+U1 = -K*(q - [qstar(:,i);qstardot(:,i)]);
 
-U2 = param.B \ (-param.A*[qstar(:,t);qstardot(:,t)] + [qstardot(:,t);qstardotdot(:,t)]);
+U2 = param.B \ (-param.A*[qstar(:,i);qstardot(:,i)] + [qstardot(:,i);qstardotdot(:,i)]);
 
-param.U = U1 + U2;
+param.U = U1 + U2 + [0;0;0;g*(m1+m2+mbh)];  %gravity cancellation
 
-param.U = param.U + [0;0;0;g*(m1+m2+mbh)];  %gravity cancellation
+param.Uplot(:,i) = param.U;
 
 if linflag
     %%feedback linearisation
@@ -177,7 +146,7 @@ if linflag
           (param.U(4))/(m1+m2+mbh) - g];
     dx = fx + gx;
 else
-    ddq = inv(param.M) * (-param.C*q(5:8) - param.G + param.U);
+    ddq = param.M \ (-param.C*q(5:8) - param.G + param.U);
     dx = [q(5:8);ddq];
 end
 %% trajectory stuff
--- Matlab/TrajectoryGeneration.m
+++ Matlab/TrajectoryGeneration.m
@@ -1,21 +1,17 @@
-function [q,qd,qdd] = TrajectoryGeneration(simspan) 
-
-wpts = [0.25 +0.5 0.5 0.25 0.25;
-        0.25 +0.25 0.5 0.5 0.25;
-        0.4 0.4 0.4 0.4 0.4];
+function [q,qd,qdd] = TrajectoryGeneration(wpts,simspan,maxvel) 
     
-[qtemp, qdtemp, qddtemp, tvec, pp] = trapveltraj(wpts, length(simspan));
+[qtemp, qdtemp, qddtemp, tvec, pp] = trapveltraj(wpts, length(simspan), 'PeakVelocity' , maxvel);
 
 q = zeros(4,size(qtemp,2));
 
 for i = 1:size(q,2)
-    q(:,i) = IKM(qtemp(:,i));
+    [q(:,i),qd(:,i)] = IKM(qtemp(:,i),qdtemp(:,i));
 end
 
 % for i = 1:size(q,2)
 %     qtemp(:,i) = IKM(q(:,i));
 % end
-qd = gradient(q);
+% qd = gradient(q);
 
 qdd = gradient(qd);
 
@@ -30,18 +26,18 @@ qdd = gradient(qd);
 % xlabel('t')
 % ylabel('Velocities')
 % legend('X','Y')
-% % 
-% figure(2)
-% subplot(2,1,1)
-% plot(tvec, q)
-% xlabel('t')
-% ylabel('Joints')
-% legend('r1','r2','theta','z')
-% subplot(2,1,2)
-% plot(tvec, qd)
-% xlabel('t')
-% ylabel('Velocities')
-% legend('X','Y')
+% 
+figure(2)
+subplot(2,1,1)
+plot(tvec, q)
+xlabel('t')
+ylabel('Joints')
+legend('r1','r2','theta','z')
+subplot(2,1,2)
+plot(tvec, qd)
+xlabel('t')
+ylabel('Velocities')
+legend('X','Y')
 % 
 % figure(3)
 % plot3(wpts(1,:),wpts(2,:),wpts(3,:))
